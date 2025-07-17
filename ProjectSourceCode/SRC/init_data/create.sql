cb97183d1174fe536d1ad665d918809556f9dce7
Joseph Webb <jowe2337@colorado.edu>
2025-07-17 15:20:48 -0600

    Adding David B's code to index.js, package.json, create.sql, register.hbs, login.hbs

--- ProjectSourceCode/SRC/init_data/create.sql
+++ ProjectSourceCode/SRC/init_data/create.sql
@@ -1,3 +1,12 @@
+-- Table for storing user information
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+
 --Table for storing workout logs
 CREATE TABLE workoutlogs (
   id INT PRIMARY KEY
